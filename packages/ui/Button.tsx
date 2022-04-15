ad1ae60f3f640580713e72d85f09484f76c94225
Jason Wiker <jason.wiker@gmail.com>
2022-04-15 16:25:14 -0600

    add basic marketing page

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,11 @@
 import * as React from "react";
 export const Button = () => {
-  return <button className="bg-blue-500">Booooop</button>;
+  return (
+    <button
+      type="submit"
+      className="block w-full rounded-md border border-transparent px-5 py-3 bg-rose-500 text-base font-medium text-white shadow hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10"
+    >
+      Notify me
+    </button>
+  );
 };
