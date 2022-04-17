f39367aa126413dd94f966da15206680d51297d9
Jason Wiker <jason.wiker@gmail.com>
2022-04-17 13:18:36 -0600

    comment out podcast api for now

--- apps/music-app/pages/api/podcastSearch.ts
+++ apps/music-app/pages/api/podcastSearch.ts
@@ -4,22 +4,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let lastfmkey = "";
-  let test = await fetch(
-    `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=${lastfmkey}&artist=${req.query.search.replaceAll(
-      `"`,
-      ""
-    )}&format=json`
-  );
-  let testjson = await test.json();
-  console.log(testjson.artist.bio.summary.replaceAll("\n", ""));
-  console.log(
-    testjson.artist.bio.summary
-      .replaceAll("\n", "")
-      .match(/(\w+\s\w+)(?=\s?\(.*?)/gm)
-  );
-  console.log(
-    testjson.artist.bio.summary.replaceAll("\n", "").match(/\((.*?)\)/gm)
-  );
+  //   let lastfmkey = "";
+  //   let test = await fetch(
+  //     `https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=${lastfmkey}&artist=${req.query.search.replaceAll(
+  //       `"`,
+  //       ""
+  //     )}&format=json`
+  //   );
+  //   let testjson = await test.json();
+  //   console.log(testjson.artist.bio.summary.replaceAll("\n", ""));
+  //   console.log(
+  //     testjson.artist.bio.summary
+  //       .replaceAll("\n", "")
+  //       .match(/(\w+\s\w+)(?=\s?\(.*?)/gm)
+  //   );
+  //   console.log(
+  //     testjson.artist.bio.summary.replaceAll("\n", "").match(/\((.*?)\)/gm)
+  //   );
   res.status(200).json({ name: "John Doe" });
 }
