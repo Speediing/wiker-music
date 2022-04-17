9cb1ff6abd9a33926a794486ddb00167d3bae80f
Jason Wiker <jason.wiker@gmail.com>
2022-04-17 13:48:57 -0600

    More Mobile Tweaks

--- packages/ui/Nav/MobileNav.tsx
+++ packages/ui/Nav/MobileNav.tsx
@@ -10,9 +10,9 @@ export interface MobileNavProps {
   options: NavOption[];
   loggedIn: boolean;
 }
-function MobileNav({ options }: MobileNavProps) {
+function MobileNav({ options, loggedIn }: MobileNavProps) {
   return (
-    <Disclosure.Panel className="lg:hidden">
+    <Disclosure.Panel className="lg:hidden bg-slate-600">
       <div className="px-2 pt-2 pb-3 space-y-1">
         {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
         {options.map((option, index) =>
@@ -39,47 +39,34 @@ function MobileNav({ options }: MobileNavProps) {
           )
         )}
       </div>
-      <div className="pt-4 pb-3 border-t border-gray-700">
-        <div className="flex items-center px-5">
-          <div className="flex-shrink-0">
-            <img
-              className="h-10 w-10 rounded-full"
-              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              alt=""
-            />
-          </div>
-          <div className="ml-3">
-            <div className="text-base font-medium text-white">Tom Cook</div>
-            <div className="text-sm font-medium text-gray-400">
-              tom@example.com
+      {loggedIn && (
+        <div className="pt-4 pb-3 border-t border-gray-700">
+          <div className="flex items-center px-5">
+            <div className="flex-shrink-0">
+              <img
+                className="h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+            </div>
+            <div className="ml-3">
+              <div className="text-base font-medium text-white">Tom Cook</div>
+              <div className="text-sm font-medium text-gray-400">
+                tom@example.com
+              </div>
             </div>
           </div>
+          <div className="mt-3 px-2 space-y-1">
+            <a
+              href="/podcast"
+              onClick={() => localStorage?.clear()}
+              className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+            >
+              Sign out
+            </a>
+          </div>
         </div>
-        <div className="mt-3 px-2 space-y-1">
-          <Disclosure.Button
-            as="a"
-            href="#"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-          >
-            Your Profile
-          </Disclosure.Button>
-          <Disclosure.Button
-            as="a"
-            href="#"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-          >
-            Settings
-          </Disclosure.Button>
-          <Disclosure.Button
-            as="a"
-            href="/podcast"
-            onClick={() => localStorage?.clear()}
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
-          >
-            Sign out
-          </Disclosure.Button>
-        </div>
-      </div>
+      )}
     </Disclosure.Panel>
   );
 }
--- packages/ui/Nav/Nav.tsx
+++ packages/ui/Nav/Nav.tsx
@@ -93,20 +93,23 @@ export const Nav = ({ options, loggedIn, loginUrl }: NavProps) => {
                   </div>
                 )}
               </>
-
-              <div className="flex lg:hidden">
-                {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
               {loggedIn && (
                 <>
+                  <div className="flex lg:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <MenuIcon
+                          className="block h-6 w-6"
+                          aria-hidden="true"
+                        />
+                      )}
+                    </Disclosure.Button>
+                  </div>
+
                   <ProfileMenu />
                 </>
               )}
