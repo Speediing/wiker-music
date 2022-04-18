d5feb26b7b97ed87b0ef1da18d1bb004b5ee5f52
Jason Wiker <jason.wiker@gmail.com>
2022-04-18 17:45:22 -0600

    Fix Build

--- apps/music-app/pages/podcast.tsx
+++ apps/music-app/pages/podcast.tsx
@@ -99,7 +99,6 @@ export default function Podcast({ data }: any) {
           </header>
           <main className="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
             <motion.div
-              className="bg-zinc-900 rounded-lg shadow px-5 py-6 sm:px-6"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               className="bg-zinc-900 shadow overflow-hidden rounded-md"
