d9763068befe0966768ef771cc25180757f9150a
Jason Wiker <jason.wiker@gmail.com>
2022-04-19 14:09:41 -0600

    Add favicon

--- apps/music-app/pages/_app.tsx
+++ apps/music-app/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { SWRConfig } from "swr";
 import "../styles/globals.css";
 import { fetcher } from "../utils/helpers/authHelpers";
@@ -6,6 +7,9 @@ import { fetcher } from "../utils/helpers/authHelpers";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig value={{ fetcher }}>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Component className={"bg-black min-h-screen h-full"} {...pageProps} />
     </SWRConfig>
   );
