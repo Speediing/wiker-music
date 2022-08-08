a5274fece62d166db7e73f48dfd11468cdf4dc6e
Jason Wiker <Jason.wiker@vercel.com>
2022-08-08 16:48:45 -0600

    Test new source

--- apps/music-app/components/SearchInput.tsx
+++ apps/music-app/components/SearchInput.tsx
@@ -8,13 +8,13 @@ export interface SearchInputProps {
 export const SearchInput = ({ onSearch, setSearch }: SearchInputProps) => {
   return (
     <div className="relative">
-      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-        <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <SearchIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
       </div>
       <input
         id="search"
         name="search"
-        className="block w-full pl-10 pr-3 py-2 border border-white rounded-md leading-5 bg-black text-gray-300 placeholder-gray-400 focus:outline-none focus:bg-black focus:border-rose-500 focus:ring-rose-500 focus:text-white sm:text-sm"
+        className="block w-full py-2 pl-10 pr-3 leading-5 text-gray-300 placeholder-gray-400 bg-black border border-white rounded-md focus:outline-none focus:bg-black focus:border-rose-500 focus:ring-rose-500 focus:text-white sm:text-sm"
         placeholder="Search"
         onKeyPress={(ev) => {
           if (ev.key === "Enter") {
